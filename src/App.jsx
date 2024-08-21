f8b1da6d90f023201e3a8a93b0c38158e97d326b
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-08-20 23:43:23 -0700

    scaffold top bar components

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ function App() {
 
   return (
     <>
-      <h1>Linear Combinatorics Visualizer</h1>
+      <div className='top-padding'>
+        <h1>Linear Combinatorics Visualizer</h1>
+
+        <div className='top-bar'>
+          <div className='notation flex-centered'>
+            <h2><span className='sub'>n</span>P<span className='sub'>r</span></h2>
+          </div>
+
+          <div className='formula flex-centered'>
+            <table>
+              <tr><td>n!</td></tr>
+              <tr><td>(n - r)!</td></tr>
+            </table>
+          </div>
+
+          <div className='slots flex-centered'>__ __ __ __ __ __ __ __ __ __</div>
+
+        </div>
+      </div>
     </>
   )
 }
