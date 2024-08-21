75b54afed780dd44f268b60d838d418fe2a9dfcb
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-08-21 13:01:34 -0700

    add input field and state

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+
+  const handleChange = event => setName(event.target.value)
 
   return (
     <>
       <div className='top-padding'>
-        <h1>Linear Combinatorics Visualizer</h1>
 
-        <div className='top-bar'>
-          <div className='notation flex-centered'>
-            <h2><span className='sub'>n</span>P<span className='sub'>r</span></h2>
+        <div className='flex flex-start flex-align-center'>
+          <h1>Linear Combinatorics Visualizer</h1>
+          <form>
+              <label htmlFor="name"></label>
+              <input value={name} onChange={handleChange} id="name" placeholder='enter a string...' />
+          </form>
+        </div>
+
+        <div className='top-bar flex'>
+          <div className='notation flex-centered flex'>
+            <h2><span className='sub'>n </span>P<span className='sub'>r</span></h2>
           </div>
 
-          <div className='formula flex-centered'>
+          <div className='formula flex-centered flex'>
             <table>
-              <tr><td>n!</td></tr>
-              <tr><td>(n - r)!</td></tr>
+              <tbody>
+                <tr><td>n!</td></tr>
+                <tr><td>(n - r)!</td></tr>
+              </tbody>
             </table>
           </div>
 
-          <div className='slots flex-centered'>__ __ __ __ __ __ __ __ __ __</div>
+          <div className='slots flex-centered flex'>__ __ __ __ __ __ __ __ __ __</div>
 
-        </div>
-      </div>
+          </div> {/*  top-bar */}
+          <h1>{name}</h1>
+      </div>  {/*  top-padding */}
     </>
   )
 }
