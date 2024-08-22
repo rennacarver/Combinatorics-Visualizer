11d4162da3742066a687f0039865e235a464faf0
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-08-21 20:16:44 -0700

    new input shorter than old input fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,72 @@ import './App.css'
 import Slot from './components/Slot/Slot';
 
 function App() {
-  const colorArray = ['#EC6769', '#80D361', '#4498C3', '#FAE75F', '#F2A664', '#EC66AB', '#B461D3', '#C36F44', '#5F72FA', '#67ECEA']
-  const randomColorArrayRef = useRef(colorArray.sort((a, b) => 0.5 - Math.random()))
-  const stringArrayRef = useRef([])
+  const colorArray = ['#EC6769', '#80D361', '#4498C3', '#FAE75F', '#F2A664', '#EC66AB', '#B461D3', '#C36F44', '#5F72FA', '#67ECEA'] 
+  const randomColorArray = colorArray.sort((a, b) => 0.5 - Math.random())
+
+  //Current data
+  const currentStringArrayRef = useRef([])
   const currentColorsRef = useRef([]);
+
+  //Previous data
+  const prevStringArrayRef = useRef([])
+  const prevColorsRef = useRef([]);
+
+  const randomColorArrayRef = useRef(randomColorArray)
+  
+  
   
 
-  const [string, setString] = useState('');
+  const [userString, setUserString] = useState('');
   const handleChange = event => {
     
      
-    setString(event.target.value)
+    setUserString(event.target.value)
     //currentColorsRef.current.push(randomColorArrayRef.current.pop())
-    console.log(`currentColors = ${currentColorsRef.current}`)
-    console.log(`randomColorArray = ${randomColorArrayRef.current}`)
+    // console.log('-----------------------------')
+    // console.log(`prevColorsRef.current = ${prevColorsRef.current}`)
+    // console.log(`currentColorsRef.current = ${currentColorsRef.current}`)
+    // console.log(`randomColorArrayRef.current = ${randomColorArrayRef.current}`)
   }
 
   useEffect(() => {
     //randomColorArray = colorArray.sort((a, b) => 0.5 - Math.random())
-    stringArrayRef.current = string.split('')
-    console.log(`stringArrayRef = ${stringArrayRef.current}`)
-  }, [string])
+
+    //Set previous state and set current state to latest input
+    console.log(`prevColorsRef.current = ${prevColorsRef.current}`)
+    console.log(`currentColorsRef.current = ${currentColorsRef.current}`)
+    prevColorsRef.current = [...currentColorsRef.current]
+    currentColorsRef.current = []
+    prevStringArrayRef.current = currentStringArrayRef.current
+    currentStringArrayRef.current = userString.split('')
+    console.log(`prevStringArrayRef = ${prevStringArrayRef.current}`)
+    console.log(`currentStringArrayRef = ${currentStringArrayRef.current}`)
+
+    //if input is empty, randomize the color array
+    if(userString.length === 0) {
+      randomColorArrayRef.current = randomColorArray
+    }
+
+    for (let i=0; i<userString.length; i++){
+      if (currentStringArrayRef.current[i] === prevStringArrayRef.current[i]){
+        console.log(`the ${i} element is the same as last time!`)
+        currentColorsRef.current.push(prevColorsRef.current[i])
+      } else{
+        console.log(`the ${i} element is the different from last time!`)
+        if(prevColorsRef.current[i]){
+          randomColorArrayRef.current.push(prevColorsRef.current[i])
+          console.log('unused color pushed back to randomcolorarray')
+        }
+          
+        currentColorsRef.current.push(randomColorArrayRef.current.pop())
+      }
+    }
+    console.log(`prevColorsRef.current = ${prevColorsRef.current}`)
+    console.log(`currentColorsRef.current = ${currentColorsRef.current}`)
+    console.log(`randomColorArrayRef.current = ${randomColorArrayRef.current}`)
+    console.log('-----------------------------')
+
+  }, [userString])
 
   return (
     <>
@@ -32,8 +77,8 @@ function App() {
         <div className='flex flex-start flex-align-center'>
           <h1>Linear Combinatorics Visualizer</h1>
           <form>
-              <label htmlFor="string"></label>
-              <input value={string} onChange={handleChange} id="string" placeholder='enter a string...' maxLength='10'/>
+              <label htmlFor="userString"></label>
+              <input value={userString} onChange={handleChange} id="userString" placeholder='enter a string...' maxLength='10'/>
           </form>
         </div>
 
@@ -52,16 +97,16 @@ function App() {
           </div>
 
           <div className='slots flex-centered flex'>
-            <Slot value={string.slice('')[0] ? string.slice('')[0].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[1] ? string.slice('')[1].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[2] ? string.slice('')[2].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[3] ? string.slice('')[3].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[4] ? string.slice('')[4].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[5] ? string.slice('')[5].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[6] ? string.slice('')[6].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[7] ? string.slice('')[7].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[8] ? string.slice('')[8].toUpperCase() : ''}></Slot>
-            <Slot value={string.slice('')[9] ? string.slice('')[9].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[0] ? userString.slice('')[0].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[1] ? userString.slice('')[1].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[2] ? userString.slice('')[2].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[3] ? userString.slice('')[3].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[4] ? userString.slice('')[4].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[5] ? userString.slice('')[5].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[6] ? userString.slice('')[6].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[7] ? userString.slice('')[7].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[8] ? userString.slice('')[8].toUpperCase() : ''}></Slot>
+            <Slot value={userString.slice('')[9] ? userString.slice('')[9].toUpperCase() : ''}></Slot>
             
           </div>
 
