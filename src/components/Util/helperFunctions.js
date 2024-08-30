07b924c2d9b7828690063ee85ac1727dd91c3bae
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-08-30 14:05:42 -0700

    enable r value for findSubsets function

--- src/components/Util/helperFunctions.js
+++ src/components/Util/helperFunctions.js
@@ -1,14 +1,16 @@
 //Generate all possible combinations from a string
 //Generate all possible subsets from an array
-export function findSubsets(string) {
+export function findSubsets(string, r) {
     const array = string.split('')
     const n = array.length
     let subsets = []
-    // Loop through all possible subsets using bit manipulation
+    // Loop through all possible subsets using bit manipulation starting at 1
     for (let i = 1; i < (1 << n); i++) {
         // Loop through all elements of the input array
         let subset = []
-        for (let j = 0; j < n; j++) {
+        //the r-value for nCr is equal to the number of 1's in the binary representation
+        if (bitCount(i) === r){
+            for (let j = 0; j < n; j++) {
             // Check if the jth bit is set in the current subset
             if ((i & (1 << j)) !== 0) {
                 // If the jth bit is set, add the jth element to the subset
@@ -16,10 +18,15 @@ export function findSubsets(string) {
             }
         }
         subsets.push(subset.join(''))
+        }
     }
     return subsets
 }
 // THIS CODE IS IN PART CONTRIBUTED TO YASH AGARWAL(YASHAGARWAL2852002)
+
+function bitCount (num) {
+    return (num >>> 0).toString(2).replaceAll("0","").length
+}
 
 //Generate an array of permutation objects
 //combination: the subset the permutation belongs to
